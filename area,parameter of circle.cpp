ea9f84fcb3ab4f2e526f1c43a59e7c1b8b61803e
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-21 16:36:30 +0530

    [ADDED]: JAYESH find the Area and Parameter of Circle

---
+++ area,parameter of circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float area,rad,peri;
+	cout<<"\n Enter the radius";
+	cin>>rad;
+	area=3.142*rad*rad;
+	cout<<"\n area of Circle:"<<area;
+	peri=2*3.142*rad;
+	cout<<" \n parameter of Circle is:"<<peri;
+}
