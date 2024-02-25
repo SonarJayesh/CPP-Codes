7574a9820bbdb3ef8e32fb8bd5cf343ea69bf885
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-25 16:01:33 +0530

    [ADDED]: JAYESh cpp code for Enput the array info

---
+++ ArrayInput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[5],i;
+	cout<<"Enter the Array Element";
+	for(i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	cout<<"Array Element is:";
+	for(i=0;i<5;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
