0419b41d1bee34c85e8d06530bbf444fc3806936
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-26 21:06:23 +0530

    [ADDED]: JAYESH Cpp code for find the Are of Trangle

---
+++ calculate area of triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float area,h,b;
+	cout<<"Enter Hight of Triangle: \n ";
+	cin>>h;
+	cout<<"Enter The Base of triagle: \n ";
+	cin>>b;
+	area=0.5*h*b;
+	cout<<"Area of trangle Is:"<<area;
+	
+	return 0;
+}
