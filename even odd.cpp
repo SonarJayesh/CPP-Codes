7d224a57d8952ff788892fb14cc94a0f88ca3069
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-18 19:05:04 +0530

    [ADDED]: JAYESH cpp code for Find the Number is Even or Odd

---
+++ even odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the two No:\n";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<n<<"is Even No \n";
+	}
+	else
+	{
+		cout<<n<<" is Odd No";
+	}
+}
