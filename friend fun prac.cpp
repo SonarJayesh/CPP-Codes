8eb87896c2b351e13e000d6123db55c1eec584e5
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-23 18:30:42 +0530

    [ADDED]: JAYESH Cpp code for Frienf Function

---
+++ friend fun prac.cpp
@@ -0,0 +1,26 @@
+//Frient function.
+
+#include<iostream>
+using namespace std;
+class sample{
+	int a,b;
+	public:
+		void setvalue(){
+			a=20;
+			b=40;
+		}
+		friend float mean (sample s);
+};
+float mean (sample s)
+{
+	return float(s.a+s.b);
+}
+int main()
+{
+	sample x;
+	x.setvalue();
+	
+	cout<<"mean value="<<mean(x);
+	
+	return 0;
+}
