51eb5f7078e5248f818396ddaf804ea42f7f5643
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-11 18:52:16 +0530

    [ADDED]: JAYESH Cpp code for Addition of 2 No using function

---
+++ function add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+	 int sum=a+b;
+}
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter the 2 No:"<<endl;
+	cin>>a>>b;
+	sum=add(a,b);
+	cout<<"sum="<<sum;
+	return 0;
+}
