85af9160542bdf3e751f141b3e7805d1e0788f40
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-16 20:07:55 +0530

    [ADDED]: JAYESH cpp code for macro function for using ternary operator

---
+++ macro func.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<windows.h>
+#define jay(a,b) ((a>b)?a:b)
+using namespace std;
+int main()
+{	
+	cout<<jay(100,500)<<endl;
+	cout<<jay(400,5260)<<endl;
+	cout<<jay(15500,5100)<<endl;
+	cout<<jay(100,50250)<<endl;
+	cout<<jay(110,10);
+	return 0;
+}
