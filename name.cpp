fe0bab867030652f2f46de024a698a913bdafa52
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-17 22:21:28 +0530

    [ADDED]: JAYESH cpp code for print the name and perform Additionof 2 No

---
+++ name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"jayesh "<<endl;
+	cout<<"sonar"<<endl;
+	int a,b,sum;
+	cout<<"Enter the two No"<<endl;
+	cin>>a >>b;
+	sum=a+b;
+	cout<<"Addition:"<<sum;
+	
+	return 0;
+}
