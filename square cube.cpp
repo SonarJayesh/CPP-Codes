62cb240367554ff5b970c9e4c7dcee95883fd5d9
SonarJayesh <jayeshsonar2824@gmail.com>
2024-04-25 21:14:03 +0530

    [ADDED]: JAYESH cpp code for find the Square root and Qube root

---
+++ square cube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,squ,cube;
+	cout<<"Enter the No:\n";
+	cin>>n;
+	squ=n*n;;
+	cout<<"\n square root of:"<<squ;
+	cube=n*n*n;
+	cout<<"\n cube root of"<<cube;
+}
