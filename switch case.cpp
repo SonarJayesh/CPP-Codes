8e236196e1acd5fd6944dfb0342f3fe417d304d0
SonarJayesh <jayeshsonar2824@gmail.com>
2024-05-01 17:22:30 +0530

    [ADDED]: JAYESH cpp code for Simple Switch case program

---
+++ switch case.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the No:";
+	cin>>n;
+	switch(n)
+	{
+		case 1:cout<<"ONE";
+		break;
+		case 2:cout<<"TWO";
+		break;
+		case 3:cout<<"THREE";
+		break;
+		case 4:cout<<"FOUR";
+		break;
+		case 5:cout<<"FIVE";
+		break;
+		default:cout<<"WRONG NUMBER";
+	}
+}
